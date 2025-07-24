2124bc311d1b778905f29d08a751549b0a4517fe
AadhyaAgrawal1234 <sweetncuteaadhya@gmail.com>
2025-07-24 18:55:03 +0530

    Add files via upload

---
+++ story.js
@@ -0,0 +1,20 @@
+function typeAndscream() {
+
+    var personName= document.getElementById("name").value;
+    alert(" I told you not to do that " + personName);
+
+}
+
+
+function godark(){
+
+document.body.className ="dark";
+
+}
+
+function finishthestory(){
+
+document.getElementById("story").innerHTML="So I caefully seached the damp basment,There You Are! "
+
+
+}
