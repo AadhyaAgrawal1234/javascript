662a103d95d65057ac8cfc3d80b5242de374c0ac
AadhyaAgrawal1234 <sweetncuteaadhya@gmail.com>
2025-07-26 11:42:58 +0530

    Add files via upload

---
+++ my hobbies.js
@@ -0,0 +1,20 @@
+function typeAndscream() {
+
+    var personName= document.getElementById("name").value;
+    alert(" I told you not to do that " + personName);
+
+}
+
+
+function godark(){
+
+document.body.className ="dark";
+
+}
+
+function finishthestory(){
+
+document.getElementById("hobbies").innerHTML=" They are dancing,reading and crafting "
+
+
+}
