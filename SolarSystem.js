450ff18b5f1fce0d30a81e99cae55f7b3f5313dd
AadhyaAgrawal1234 <sweetncuteaadhya@gmail.com>
2025-07-30 18:10:50 +0530

    Add files via upload

---
+++ SolarSystem.js
@@ -0,0 +1,10 @@
+var div= document.createElement('div');
+
+div.id = "paragraph"
+
+div.className = "alert";
+
+div.innerHTML = "<p>The solar system is a collection of celestial objects gravitationally bound to the Sun, a star at the center. It includes eight planets, dwarf planets, moons, asteroids, comets, and other smaller objects like dust. The solar system is located in the Orion Arm of the Milky Way galaxy.  </p>"
+
+document.body.append(div);
+
